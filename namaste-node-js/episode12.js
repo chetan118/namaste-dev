8ccf7dbcf14d4c0cab759c353e3d23d7eb573862
chetan118 <chetan.sakpal@gmail.com>
2025-06-12 01:24:26 +0530

    node - Databases - SQL & NoSQL
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

---
+++ namaste-node-js/episode12.js
@@ -0,0 +1,31 @@
+// episode12.js
+/*
+Database
+- organized collection of data or type of data store
+
+DBMS
+- the software that interacts with end users, applications and the database itself 
+to capture and analyze the data
+- encompasses the core facilities provided to administer the database
+
+Different Types
+mostly used are relational dbs, No SQL, in memory (redis), cloud (Amazon RDS)
+
+Types of No SQL DBs (Not Only SQL)
+- Document DB (Mongo DB)
+- Key Value DB
+- Graph DB
+- Wide Column DB
+- Multi Model DB
+
+RDBMS vs NoSQL
+- Tables, Columns & Rows - Collection, document & fields
+- Structured data - Unstructured data
+- Fixed Schema - Flexible Schema
+- SQL - Mongo (MQL), Neo4j (cypher)
+- Tough horizontal scaling - Easy to scale horizontally & vertically
+- Restionships (foreign keys + joins) - Relationships (nested)
+- Read heavy apps, transaction workload - Real time, big data, distributed computing
+- Ex. Banking apps - Ex. Real time analysis, social media
+
+*/
