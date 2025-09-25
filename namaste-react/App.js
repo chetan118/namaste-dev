eff7609805b7731ac7344d0747aca95b02a12592
Chetan Sakpal <chetan.sakpal@gmail.com>
2025-09-25 11:17:47 +0530

    React Episode 3 - Multiline jsx
    Signed-off-by: Chetan Sakpal <chetan.sakpal@gmail.com>

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => return ReactElement i.e. JS Object => on rendering HTMLElement
-
-const heading = React.createElement("h1", { id: "heading", key: "heading" }, "Hello World from React! 🚀");
-console.log(heading);
-
 // JSX - JavaScript syntax to help with React
 // It is not HTML in JS
 // It is HTML-like or XML-like syntax
 // JS Engine will not understand this code as-is
 // Parcel uses Babel package to transpile the code to JS before it reaches the JS Engine
 // JSX Code => transpiled to React.createElement => return ReactElement i.e. JS Object => on rendering HTMLElement
-const jsxHeading = <h1 id="heading" key="jsxHeading">Namaste React using JSX 🚀</h1>;
+// JSX attributes are camelCase
+const jsxHeading = (
+  <h1 id="heading" key="jsxHeading" tabIndex={5}>
+    Namaste React using JSX 🚀
+  </h1>
+);
 console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- namaste-react/Notes/S01E03 - Laying the foundation.md
+++ namaste-react/Notes/S01E03 - Laying the foundation.md
@@ -1,1 +1,12 @@
 # Episode-03 | Laying the foundation
+
+## JSX
+
+- It is a syntax to help with React
+- It is not HTML in JS
+- It is HTML-like or XML-like syntax
+- JS Engine will not understand this code as-is
+- Parcel uses Babel package to transpile the code to JS before it reaches the JS Engine
+- JSX Code => transpiled to React.createElement => returns ReactElement i.e. JS Object => on rendering HTMLElement
+- JSX attributes are camelCase
+- H.W. - image src, a href in JSX?
