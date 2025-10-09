96a2aa36b36a4c60d84d70c0888652c1fd453089
chetan118 <chetan.sakpal@gmail.com>
2025-10-09 18:20:27 +0530

    React - S01E05 - Let's Get Hooked
    - Using React state variable to filter top rated restaurants
    
    Signed-off-by: chetan118 <chetan.sakpal@gmail.com>

--- namaste-react/Notes/S01E05 - Let's Get Hooked.md
+++ namaste-react/Notes/S01E05 - Let's Get Hooked.md
@@ -10,5 +10,22 @@
 ## React Hooks
 
 - Normal JS utility function - written by facebook devs in the react library
-- useState() - superpowerful state variables in React
+- useState()
+  - super powerful state variables in React
+  - _WHENEVER A STATE VARIABLE UPDATES, REACT WILL RE-RENDER THE COMPONENT_
 - useEffect()
+
+## Re-rendering - Behind the scenes
+
+### Reconciliation Algorithm (React Fiber)
+
+- DOM (Document Object Model) is like a tree.
+- Suppose the "restaurant-container" node currently had 7 restaurnat-card nodes under it
+- and upon filtering the data changes and we only have 3 restaurant-card nodes now
+- React creates a virtual DOM of the component
+- Virtual DOM is a respresentation of an actual DOM (i.e. React Element - i.e. JavaScript object)
+
+### Diff Algorithm
+
+- In React16, the new algorithm for calculating the difference between the two virtual DOMs came in.
+- It calculates the diff and rerenders the component
--- namaste-react/src/components/Body.js
+++ namaste-react/src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // Local state variable - super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredListOfRestaurants = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredListOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
